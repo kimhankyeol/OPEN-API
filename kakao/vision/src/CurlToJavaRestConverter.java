7950794daad68b3d47a99da574e1ab882c7fd080
kimhankyeol <41567215+kimhankyeol@users.noreply.github.com>
2020-09-16 17:13:21 +0900

    Update CurlToJavaRestConverter.java

--- kakao/vision/src/CurlToJavaRestConverter.java
+++ kakao/vision/src/CurlToJavaRestConverter.java
@@ -5,6 +5,7 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.HttpClientBuilder;
 
@@ -84,7 +85,8 @@ public class CurlToJavaRestConverter {
                 e.printStackTrace();
                 System.err.println(e.toString());
             }
-    
-    }
+        }
     return this.body;
+    }
+    
 }
